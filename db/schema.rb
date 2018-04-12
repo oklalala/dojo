6b995d3df86e817c07827caea329b8ecc08bbfa3
Tony Lin <tonylin1208@gmail.com>
2018-04-12 17:41:01 +0800

    add collect model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_12_090421) do
+ActiveRecord::Schema.define(version: 2018_04_12_094033) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collects", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
