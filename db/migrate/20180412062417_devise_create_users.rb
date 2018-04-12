a75ff1029caa0d29a880520ecbd1abb9f8a87290
Tony Lin <tonylin1208@gmail.com>
2018-04-12 14:37:49 +0800

    set user attribute with migrate

--- db/migrate/20180412062417_devise_create_users.rb
+++ db/migrate/20180412062417_devise_create_users.rb
@@ -22,16 +22,21 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string   :last_sign_in_ip
 
       ## Confirmable
+      # custamize the attribute.
+      t.string :name, null: false, default: ""
+      t.string :avatar
+      t.text :intro
+      t.string :role
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.integer  :failed_attempts, default: 0, null: false
+      # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
