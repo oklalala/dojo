db2ce48787e5881e55bf39e294ed88edb0f09667
Tony Lin <tonylin1208@gmail.com>
2018-04-12 16:55:07 +0800

    fix strong params in admin user controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,34 +1,36 @@
-class Admin::UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :authenticate_admin
-  def index
-    @users = User.all
+module Admin
+  class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :authenticate_admin
+    def index
+      @users = User.all
 
-    return @user = User.find(params[:id]) if params[:id]
-    @user = User.new
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to admin_users_path
-      flash[:notice] = 'user was successfully updated'
-    else
-      @users = user.all
-      render :index
+      return @user = User.find(params[:id]) if params[:id]
+      @user = User.new
     end
-  end
 
-  private
-
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = 'Not allow!'
-      redirect_to root_path
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to admin_users_path
+        flash[:notice] = 'user was successfully updated'
+      else
+        @users = user.all
+        render :index
+      end
     end
-  end
 
-  def user_params
-    params.require(:user).permit(:name, :role)
+    private
+
+    def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = 'Not allow!'
+        redirect_to root_path
+      end
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :role, :avatar)
+    end
   end
 end
