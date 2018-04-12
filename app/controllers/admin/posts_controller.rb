b9e845f7bb89c5e3e7d6557a2e1543eb5be75b68
Tony Lin <tonylin1208@gmail.com>
2018-04-12 15:07:06 +0800

    set up admin authentication

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,6 +3,17 @@
 module Admin
   # say someting
   class PostsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :authenticate_admin
     def index; end
+
+    private
+
+    def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = 'Not allow!'
+        redirect_to root_path
+      end
+    end
   end
 end
