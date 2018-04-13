a8e068d0c6b43e88ef9d7cad63b3e747c4547827
Tony Lin <tonylin1208@gmail.com>
2018-04-13 13:49:47 +0800

    add relation of model followship and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# hahaha
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -7,8 +8,30 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, PhotoUploader
+  # user name can't be the same or pop out the error.
+  validates_presence_of :name
+  # can't_follow_yourself
+  validates :name, uniqueness: { case_sensitive: true,
+                                 message: 'has already been taken' }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  has_many :collects, dependent: :destroy
+  has_many :collected_posts, through: :collects
+
+  # Following Follower
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
+  has_many :inverse_followships, class_name: 'Followship',
+                                 foreign_key: 'friend_id'
+  has_many :followers, through: :inverse_followships, source: :user
 
   def admin?
     role == 'admin'
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
