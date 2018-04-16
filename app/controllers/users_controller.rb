ece4090ecea450b269f2b53ee59777c1bd243ff1
Tony Lin <tonylin1208@gmail.com>
2018-04-16 17:13:25 +0800

    rails g controller users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show comment collect draft friend]
+  def show
+    # @post = @user.posted
+  end
+
+  def comment; end
+
+  def collect; end
+
+  def draft; end
+
+  def friend; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :intro, :avatar)
+  end
+end
