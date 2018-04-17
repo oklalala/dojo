5c459e1924f8f05518541fe192ac7aa6a972ce00
Tony Lin <tonylin1208@gmail.com>
2018-04-17 09:09:44 +0800

    add routes of comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :categories, only: :show
   resources :posts
   root 'posts#index'
-
+  resources :comment
   resources :users do
     collection do
       get :feeds
