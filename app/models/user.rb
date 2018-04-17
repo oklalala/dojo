e02a9c8995045882b2c07e3a8e836ca7131f4d59
Tony Lin <tonylin1208@gmail.com>
2018-04-17 11:01:56 +0800

    fix relation between user and post through comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   has_many :comments, dependent: :destroy
-  has_many :commented_post, through: :comments, source: :comments
+  has_many :commented_posts, through: :comments, source: :post
 
   has_many :collects, dependent: :destroy
   has_many :collected_posts, through: :collects
