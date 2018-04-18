de0da6110b1e62e2b469789c7cc1f3d3e5649db1
Tony Lin <tonylin1208@gmail.com>
2018-04-18 16:32:51 +0800

    add create in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
+# rubocop
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "comment was successfully created"
+    else
+      flash.now[:alert] = "Did you miss something?"
+      @users = User.order(followers_count: :desc).limit(10)
+      @comments = comment.order(created_at: :desc)
+    end
+    redirect_to comments_path
+  end
+
+  private
+
+  def comment_params
+    # puts "enter the strong params"
+    params.require(:comment).permit(:content)
+  end
 end
