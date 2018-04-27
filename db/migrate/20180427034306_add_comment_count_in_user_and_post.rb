a74c6dc8c9f7c5caaa102828cb28fec937039426
Tony Lin <tonylin1208@gmail.com>
2018-04-27 12:43:43 +0800

    add column in post and user with comment_count

---
+++ db/migrate/20180427034306_add_comment_count_in_user_and_post.rb
@@ -0,0 +1,6 @@
+class AddCommentCountInUserAndPost < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :comment_count, :integer, default: 0
+    add_column :users, :comment_count, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_051135) do
+ActiveRecord::Schema.define(version: 2018_04_27_034306) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2018_04_25_051135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "comment_count", default: 0
   end
 
   create_table "sorts", force: :cascade do |t|
@@ -104,6 +105,7 @@ ActiveRecord::Schema.define(version: 2018_04_25_051135) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comment_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
