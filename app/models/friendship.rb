96c81848664f6b04039aaea9abb12cbfd017b50b
Tony Lin <tonylin1208@gmail.com>
2018-04-28 01:18:54 +0800

    rails g model friendship

---
+++ app/models/friendship.rb
@@ -0,0 +1,5 @@
+# friendship
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_034306) do
+ActiveRecord::Schema.define(version: 2018_04_27_152522) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2018_04_27_034306) do
   end
 
   create_table "followships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.boolean "accept"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
     t.boolean "accept"
