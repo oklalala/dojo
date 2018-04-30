2c9b99cf07abcf2052a3b28371fb17060677945d
Tony Lin <tonylin1208@gmail.com>
2018-04-30 14:15:03 +0800

    fix the ransack in category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+# Category
 class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.page(params[:page]).per(10)
+    @search = @category.posts.ransack(params[:q])
+    @posts = @search.result.page(params[:page]).per(10)
   end
 end
