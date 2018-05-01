890b788c99f89ebfa78476bb91fa1abca0c5ad27
Tony Lin <tonylin1208@gmail.com>
2018-05-01 22:55:14 +0800

    add index show create api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,50 @@
-class Api::V1::PostsController < ApplicationController
+class Api::V1::PostsController < ApiController
+  def index
+    @posts = Post.all
+    render json: {
+      data: @posts.map do |post|
+        {
+          title: post.title,
+          photo: post.photo,
+          content: post.content
+        }
+      end
+    }
+  end
+
+  def show
+    @photo = Photo.find_by(id: params[:id])
+    if !@photo
+      render json: {
+        message: "Can't find the photo!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @post.title,
+        photo: @post.photo,
+        content: @post.content
+      }
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: {
+        message: 'post created successfully!',
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :photo, :content)
+  end
 end
