71026a1269260c83ef23e6b1fb803b09f91f0614
Tony Lin <tonylin1208@gmail.com>
2018-05-01 23:34:44 +0800

    set routes for api auth login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,8 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
       resources :articles, only: %i[index create show update destroy]
     end
   end
