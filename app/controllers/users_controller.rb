041e9ed871d3ce9c8513239adda961109a63bef6
Tony Lin <tonylin1208@gmail.com>
2018-05-09 20:43:28 +0800

    fix you can't see yourself in  user#friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@
 # @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show comment collect draft friend]
+  def index
+    @users = User.all
+  end
+  
   def show
     @posts = @user.posts.all
   end
